f0749ea4cd7976133c316fc959c712462bc61325
FaizalFeng <fzx401@gmail.com>
2025-01-02 21:43:32 +0800

    🐛 fix bugs

--- src/dl_pipeline/pipelines/demo/nodes.py
+++
@@ -1,4 +0,0 @@
-"""
-This is a boilerplate pipeline 'demo'
-generated using Kedro 0.19.9
-"""
--- src/dl_pipeline/settings.py
+++ src/dl_pipeline/settings.py
@@ -3,10 +3,10 @@ from the Kedro defaults. For further information, including these default values
 https://docs.kedro.org/en/stable/kedro_project_setup/settings.html."""
 
 # Instantiated project hooks.
-from dl_pipeline.hooks import SparkHooks  # noqa: E402
+# from dl_pipeline.hooks import SparkHooks  # noqa: E402
 
 # Hooks are executed in a Last-In-First-Out (LIFO) order.
-HOOKS = (SparkHooks(),)
+# HOOKS = (SparkHooks(),)
 
 # Installed plugins for which to disable hook auto-registration.
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
@@ -36,9 +36,9 @@ CONFIG_LOADER_CLASS = OmegaConfigLoader
 CONFIG_LOADER_ARGS = {
     "base_env": "base",
     "default_run_env": "local",
-    "config_patterns": {
-        "spark": ["spark*", "spark*/**"],
-    }
+    # "config_patterns": {
+    #     "spark": ["spark*", "spark*/**"],
+    # },
 }
 
 # Class that manages Kedro's library components.
